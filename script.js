9dd8a70e7bec7ffff7d1b649418643f76957b4cc
reverie-palace <74068078+reverie-palace@users.noreply.github.com>
2021-09-17 15:50:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function teste3() {
+  document.write("<h1>Abrindo pagina</h1>");
+  window.open("https://discord.gg/mYMKSKtrFE");
+  window.location.href = "https://reverie-palace.github.io";
+}
+
+function teste4() {
+  window.open(
+    "https://cdn.discordapp.com/attachments/830560948905443358/888487749286527026/unknown.png"
+  );
+}
+
+function teste5() {
+  window.open(
+    "https://cdn.discordapp.com/attachments/830560948905443358/888487935396171847/unknown.png"
+  );
+}
